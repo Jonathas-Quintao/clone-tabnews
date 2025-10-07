867fdd8bc495a1daf2592e1cd67e2d5d7e67485b
Jonathas <jonathasquintaosilva2002@gmail.com>
2025-10-06 22:15:49 -0300

    add `.env` and `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
+import database from "../../../../infra/database";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
   return res.status(200).json({ chave: "toma esse json" });
 }
