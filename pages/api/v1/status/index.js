70ba4b53170b1156dac29c47945207757f6f2a89
Jonathas <jonathasquintaosilva2002@gmail.com>
2025-10-16 17:21:00 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,32 @@
 import database from "infra/database";
 
-export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  return res.status(200).json({ chave: "toma esse json" });
+export default async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query("SELECT version();");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const parseDatabaseVersionResult =
+    databaseVersionResult.rows[0].version.split(" ")[1];
+
+  const parseMaxConnections =
+    maxConnections.rows[0].max_connections.split(" ")[0];
+
+  const parseOpenedConnections = openedConnections.rows[0].count;
+
+  response.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: parseDatabaseVersionResult,
+        max_connections: parseInt(parseMaxConnections),
+        opened_connections: parseInt(parseOpenedConnections),
+      },
+    },
+  });
 }
